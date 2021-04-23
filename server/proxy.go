3155663b83fef2c53aaeb88ea300cd7efbf652e2
Gaetan <gaetan@reseau.rip>
2021-04-23 10:00:28 +0200

    Proxy: more generic logging

--- server/proxy.go
+++ server/proxy.go
@@ -17,20 +17,7 @@ import (
 
 func addBlockList(proxy *goproxy.ProxyHttpServer, message string, list domains.DomainTree, logger *log.Entry) *goproxy.ProxyHttpServer {
 	proxy.OnRequest(domains.DstHostIsIn(list)).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		ip, port := utils.GetConnection(ctx.Req.RemoteAddr)
-		logger.WithFields(log.Fields{
-			"src":          ip.String(),
-			"src_port":     port,
-			"method":       ctx.Req.Method,
-			"url":          ctx.Req.URL.String(),
-			"user_agent":   ctx.Req.Header.Get("User-Agent"),
-			"referrer":     ctx.Req.Header.Get("Referer"),
-			"action":       "block",
-			"status":       http.StatusForbidden,
-			"bytes_in":     ctx.Req.ContentLength,
-			"bytes_out":    len(message),
-			"content_type": ctx.Req.Header.Get("Content-Type"),
-		}).Error(message)
+		prepareRequestLogger(logger, ctx).Error(message)
 		return req, goproxy.NewResponse(req,
 			goproxy.ContentTypeText, http.StatusForbidden,
 			message)
@@ -59,6 +46,37 @@ func connectTestPort(portString string, global *configuration.GlobalConfig) bool
 		return false
 	}
 	return true
+}
+
+var logHeaders = map[string]string{
+	"User-Agent":   "user_agent",
+	"Referer":      "referrer",
+	"Content-Type": "content_type",
+}
+
+func prepareRequestLogger(logger *log.Entry, ctx *goproxy.ProxyCtx) *log.Entry {
+	ip, port := utils.GetConnection(ctx.Req.RemoteAddr)
+	requestLogger := logger.WithFields(log.Fields{
+		"src":      ip.String(),
+		"src_port": port,
+		"method":   ctx.Req.Method,
+		"url":      ctx.Req.URL.String(),
+		"action":   "pass",
+		"bytes_in": ctx.Req.ContentLength,
+	})
+	for header, logField := range logHeaders {
+		field := ctx.Req.Header.Get(header)
+		if len(field) > 0 {
+			requestLogger = requestLogger.WithField(logField, field)
+		}
+	}
+	if ctx.Resp != nil {
+		requestLogger = requestLogger.WithFields(log.Fields{
+			"status":    ctx.Resp.StatusCode,
+			"bytes_out": ctx.Resp.ContentLength,
+		})
+	}
+	return requestLogger
 }
 
 type ProxyServer struct {
@@ -111,27 +129,9 @@ func NewProxy(iface configuration.InterfaceConfig, global *configuration.GlobalC
 		proxy = addBlockList(proxy, "Blocked by global policy", global.BlockList, proxyLogger)
 	}
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-		ip, port := utils.GetConnection(ctx.Req.RemoteAddr)
-		requestLogger := proxyLogger.WithFields(log.Fields{
-			"src":          ip.String(),
-			"src_port":     port,
-			"method":       ctx.Req.Method,
-			"url":          ctx.Req.URL.String(),
-			"user_agent":   ctx.Req.Header.Get("User-Agent"),
-			"referrer":     ctx.Req.Header.Get("Referer"),
-			"action":       "pass",
-			"bytes_in":     ctx.Req.ContentLength,
-			"content_type": ctx.Req.Header.Get("Content-Type"),
-		})
-		if ctx.Resp != nil {
-			requestLogger = requestLogger.WithFields(log.Fields{
-				"status":    ctx.Resp.StatusCode,
-				"bytes_out": ctx.Resp.ContentLength,
-			})
-		} else {
-			requestLogger.WithFields(log.Fields{
-				"action": "error",
-			}).Error("Proxy request: no response")
+		requestLogger := prepareRequestLogger(proxyLogger, ctx)
+		if ctx.Resp == nil {
+			requestLogger.WithField("action", "error").Error("Proxy request: no response")
 			return resp
 		}
 		requestLogger.Info("Proxy request")
